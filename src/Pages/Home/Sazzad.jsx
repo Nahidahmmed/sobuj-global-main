afc67da6dcb15fbf749b95f1da9ed2ade87f1c1e
sazzad46031 <git config --global user.email sazzadmgbs@gmail.com>
2024-07-06 20:02:56 +0600

    second commit

--- src/Pages/Home/Sazzad.jsx
+++ src/Pages/Home/Sazzad.jsx
@@ -4,6 +4,7 @@ const Sazzad = () => {
     return (
         <div>
             <h2>Hello Sazzad</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ducimus dignissimos eius facilis ad minus inventore soluta enim quibusdam corporis alias placeat repellat odio, nemo nesciunt doloremque harum nihil fugit.</p>
         </div>
     );
 };
