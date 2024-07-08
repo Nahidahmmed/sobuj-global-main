3b52f54ae1e948c26435ccf9cde79716a633c72e
Nahidahmmed <nahidahmmed411@gmail.com>
2024-07-08 19:07:37 +0600

    changes on component

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Layout = () => {
     return (
         <div>
+            <Navbar/>
             <Outlet/>
             <Footer/>
         </div>
--- src/Pages/Home/Component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Component = () => {
-  return <div>This is Sujit's Component.</div>;
-};
-
-export default Component;
